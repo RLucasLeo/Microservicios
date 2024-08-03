90c6eda96e70c1f8be9f00ecfc6ac23b459418fc
RLucasLeo <lucas3leo94@gmail.com>
2024-08-02 22:16:19 -0300

    gateway config agregada

---
+++ gateway-service/src/test/java/com/microserv/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserv.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
